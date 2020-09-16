f4cbdfde2011a793e8540acd73da1a05a8b0a8c4
Pedro <phls@cin.ufpe.br>
2020-09-16 01:27:08 -0300

    deviation

--- eval/CalculadoraTCP.cs
+++ eval/CalculadoraTCP.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading;
 using System.Net.Sockets;
 using System.Data;
+using System.Collections.Generic;
 
 namespace eval
 {
@@ -13,34 +14,64 @@ namespace eval
         private static IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
         private static IPAddress iPAddress = ipHost.AddressList[0];
         private static IPEndPoint localEndPoint = new IPEndPoint(iPAddress, PORT);
+        private static int qtdClients = 1;
+        private static int qtdIteracoes = 10000;
+
+        public static long amountOfTimeEllapsed = 0;
+        public static List<long> executions = new List<long>();
 
         public static void runCalculadoraTcp()
         {
+            Thread[] clients = new Thread[qtdClients];
             Thread server = new Thread(executeServer);
             server.Start();
-            for(var i = 0; i < 1000; i++){
-                Thread client = new Thread(executeClient);
-                client.Start(i.ToString() + "2*2");
+            for(var i = 0; i < qtdClients; i++){
+                clients[i] = new Thread(executeClient);
+                clients[i].Start(i);
             }
+            for(var i = 0; i < qtdClients; i++){
+                clients[i].Join();
+            }
+            Console.WriteLine("Total time ellapsed: {0} in milliseconds", amountOfTimeEllapsed);
+            var mediaOfTimeReq = (double)amountOfTimeEllapsed/(double)10000/(double)qtdClients;
+            Console.WriteLine("Media of time by requisition: {0} in milliseconds", mediaOfTimeReq);
+            double deviation = 0;
+            foreach(var i in executions){
+                deviation += Math.Pow((i - mediaOfTimeReq),2);
+            }
+            deviation /= (double)10000/(double)qtdClients;
+            deviation = Math.Sqrt(deviation);
+            Console.WriteLine("Deviation: {0}", deviation);
         }
 
         private static void executeClient(Object obj)
         {
-            // criando socket de conexão
-            Socket sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            try{
-                sender.Connect(localEndPoint);
-                var bytesToTransfer = Encoding.ASCII.GetBytes(obj.ToString());
-                Console.WriteLine("Cliente enviando: {0}", obj.ToString() );
-                sender.Send(bytesToTransfer);
+            for(var i = 0; i < qtdIteracoes; i++){
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                // criando socket de conexão
+                Socket sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try{
+                    sender.Connect(localEndPoint);
+                    var texto = i.ToString() + "+2*2";
+                    var bytesToTransfer = Encoding.ASCII.GetBytes(texto);
+                    // Console.WriteLine("Cliente enviando: {0}", texto.ToString() );
+                    sender.Send(bytesToTransfer);
 
-                byte[] bytes = new Byte[1024]; 
-                int bytesReceived = sender.Receive(bytes);
-                Console.WriteLine("Cliente recebeu: {0}",Encoding.ASCII.GetString(bytes));
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
-            }catch(Exception e){
-                Console.WriteLine(e.Message);
+                    byte[] bytes = new Byte[1024]; 
+                    int bytesReceived = sender.Receive(bytes);
+                    // Console.WriteLine("Cliente recebeu: {0}",Encoding.ASCII.GetString(bytes));
+                }catch(Exception e){
+                    Console.WriteLine(e.Message);
+                }finally
+                {
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                    watch.Stop();
+                }
+                if((int)i == 0){
+                    executions.Add(watch.ElapsedMilliseconds);
+                    amountOfTimeEllapsed += watch.ElapsedMilliseconds;
+                }
             }
         }
 
@@ -70,10 +101,10 @@ namespace eval
             var client = (Socket)obj;
             var numBytes = client.Receive(bytes);
             data = Encoding.ASCII.GetString(bytes, 0, numBytes);
-            Console.WriteLine("Servidor recebeu: {0}",data);
+            // Console.WriteLine("Servidor recebeu: {0}",data);
             try{
                 var retorno = calculaCoisa(data);
-                Console.WriteLine("Servidor devolvendo: {0}",retorno);
+                // Console.WriteLine("Servidor devolvendo: {0}",retorno);
                 var bytesToTransfer = Encoding.ASCII.GetBytes(retorno);
                 client.Send(bytesToTransfer);
             }catch(Exception e){
