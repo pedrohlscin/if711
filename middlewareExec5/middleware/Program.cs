561011e2623369f7b1a96a058f9af2584da9157c
Pedro Henrique <phls@cin.ufpe.br>
2020-10-28 20:57:29 -0300

    exec5 bone

---
+++ middlewareExec5/middleware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace middleware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
