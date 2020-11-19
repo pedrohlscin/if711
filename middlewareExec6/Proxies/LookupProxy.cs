219c61c7c0162bf2fc6062b6d763955f214eddcb
Pedro Henrique <phls@cin.ufpe.br>
2020-11-19 01:07:06 -0300

    proxy de name lookup

---
+++ middlewareExec6/Proxies/LookupProxy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace middlewareExec6.Proxies
+{
+    class LookupProxy
+    {
+        public Dictionary<String, AOR> proxies;
+
+        private LookupProxy() 
+        {
+            proxies = new Dictionary<String, AOR>();
+        }
+
+        private static LookupProxy _instance;
+
+        private static readonly object _lock = new object();
+
+        public static LookupProxy GetInstance()
+        {
+            if(_instance == null)
+            {
+                lock (_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new LookupProxy();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public AOR GetProxy(String proxy)
+        {
+            AOR proxyDesired;
+            if (!proxies.TryGetValue(proxy, out proxyDesired))
+            {
+                return null;
+            }
+            return proxyDesired;
+        }
+    }
+}
